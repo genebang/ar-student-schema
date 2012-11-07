9f946574c65562bf982c84b00e1c8a5ce16aa71b
Gene Bang <genebang@gmail.com>
2012-11-06 17:18:36 -0800

    ar student schmea challenges done to pass testss

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,21 @@
 require_relative '../../db/config'
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+  
+  attr_accessor :birthday
+  
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :email, :uniqueness => true
+  validates :age, :numericality => { :greater_than => 5 }
+  validates :phone, :format => { :with => /.*\d{3}.*\d{3}.*\d{4}.*/ }
+  
+  def name
+    "#{first_name} #{last_name}"
+  end
+  
+  def age
+    Time.now.year - birthday.year
+  end
+  
+end
